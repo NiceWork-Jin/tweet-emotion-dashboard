b79b06b5b4955957b7e244e1b49fdf82018976fe
SeongjinLee <dev_seongjin@naver.com>
2021-11-19 15:30:55 +0900

    changed the method of p/n/n

--- flask_dashboard/static/js/FakeData_generator/fakeData_sentimentScore_TimeSeries.js
+++ flask_dashboard/static/js/FakeData_generator/fakeData_sentimentScore_TimeSeries.js
@@ -15,7 +15,7 @@ function scoreTimeSeries() {
 
 //  get average of positive/neutral/negative number
     let total = positive_num + neutral_num + negative_num;
-    let sumWithWeight = (1 * positive_num) + (-1 * negative_num);
+    let sumWithWeight = (100 * positive_num) + (50 * neutral_num) + (0 * negative_num);
     let score = (sumWithWeight / total).toFixed(2);
 
 //  Serve data maximum of 60 seconds
